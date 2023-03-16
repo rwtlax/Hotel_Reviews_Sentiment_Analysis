7dbd1f88351890018c19486c2ba9280e8a98383c
rwtlax <67238731+rwtlax@users.noreply.github.com>
2023-03-17 00:15:10 +0530

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -91,9 +91,9 @@ st.markdown("-------------------------------------------------------------------
 st.subheader("Word Cloud for Reviews Sentiment")
 
 word_ls = ['Room Size','room,','cleanliness','staff','food','AC','expensive','location','service','bathroom','noise',
-	   'noisy','free','lounge','will','never','really','not','no','comfort','comfortable','swimming pool','dirty','overpriced','ambiance','check-in',
+	   'noisy','free','lounge','will','never','really','comfort','comfortable','swimming pool','dirty','overpriced','ambiance','check-in',
 	   'check in','check out','check-out','toilet','affordable','lift','smell','complimentary','professional','friendly','amazing','good','excellent',
-	   'bad','pathetic','disappoint','rude','negative','cramped','broken','uncomfortable','positive','not','no','awful','price','was','my','of','a','The','the']
+	   'bad','pathetic','disappoint','rude','negative','cramped','broken','uncomfortable']
 
 data['Reviews1'] = data['Reviews'].apply(lambda x: ' '.join([word for word in str(x).split() if word.lower() not in (word_ls)]))
 data['Reviews1'] = data['Reviews1'].str.replace('hotel',' ')
